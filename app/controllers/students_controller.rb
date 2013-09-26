0024f10fc257138f5e71b6f96851ba7bd07dc6be
Joe Marshall <joseph.charles.marshall@gmail.com>
2013-09-26 15:24:50 -0500

    Adds dynamic string functionality

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,59 @@
+class StudentsController < ApplicationController
+
+  def show_all
+  end
+
+  def show
+    @data = {
+      'harsh' => {
+        name: "Harsh",
+        city: "St.Louis",
+        description: "The Indian man with glasses",
+        picture_url: "http://goo.gl/GwwMqe"
+      },
+      'matt' => {
+        name: "Matt Buck",
+        city: "Antarctica",
+        description: "Reigning rails ninja",
+        picture_url: "http://goo.gl/GwwMqe"
+      },
+       'ravi' => {
+        name: "Ravi",
+        city: "Everywhere, U.S.A.",
+        description: "Co-founder and general badass",
+        picture_url: "http://goo.gl/GwwMqe"
+      }
+    }
+    instructor_name = params[:username]
+    @specific_data = @data[instructor_name]
+  end
+
+
+end
+
+  # def show_harsh
+  #   @student = {
+  #       name: "Harsh Patel",
+  #       city: "St. Louis",
+  #       description: "The Indian man with glasses",
+  #       picture_url: "http://goo.gl/GwwMqe"
+  #   }
+  # end
+
+  # def show_matt
+  #   @student = {
+  #       name: "Matt Buck",
+  #       city: "Antarctica",
+  #       description: "Reigning rails ninja",
+  #       picture_url: "http://goo.gl/GwwMqe"
+  #   }
+  # end
+
+  # def show_ravi
+  #   @student = {
+  #       name: "Ravi",
+  #       city: "Everywhere, U.S.A.",
+  #       description: "Co-founder and general badass",
+  #       picture_url: "http://goo.gl/GwwMqe"
+  #   }
+  # end
